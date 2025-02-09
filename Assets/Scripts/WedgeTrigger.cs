c4ff04b709625ac450449899a3afcb0e59a296f3
Veera Raita <fvera003@edu.xamk.fi>
2025-02-09 22:29:59 +0200

    hw done

--- Assets/Scripts/WedgeTrigger.cs
+++ Assets/Scripts/WedgeTrigger.cs
@@ -5,6 +5,7 @@ public class WedgeTrigger : MonoBehaviour
 {
     [Range(0.1f, 20f)] [SerializeField] private float radius = 5.0f;
     [Range(1f, 180f)] [SerializeField] private float threshold = 45f;
+    [Range(0.1f, 2f)] [SerializeField] private float wedgeHeight = 0.5f;
 
     [SerializeField] private Transform lookTarget;
     [SerializeField] private Transform targetTransform;
@@ -29,7 +30,7 @@ public class WedgeTrigger : MonoBehaviour
         MyDraw.DrawVectorAt(vecTriggerPos, vecTargetToTrigger, Color.cyan, 3.0f);
 
         //determine arc color
-        if (vecTargetToTrigger.magnitude <= radius && angle <= threshold)
+        if (vecTargetToTrigger.magnitude <= radius && angle <= threshold && Mathf.Abs(vecTargetPos.y) < vecTriggerPos.y + wedgeHeight)
         {
             Handles.color = Color.red;
         }
@@ -38,13 +39,28 @@ public class WedgeTrigger : MonoBehaviour
             Handles.color = Color.white;
         }
 
-        //Draw arc && lines
+
+        //Draw wedge
+
+        //math for determining arc positioning
         Quaternion rotate = Quaternion.Euler(0, threshold, 0);
         Vector3 rangeRight = rotate * lookVectorNormalized;
-        Handles.DrawLine(transform.position, transform.position + rangeRight * radius, 3.0f);
         rotate = Quaternion.Euler(0, -threshold, 0);
         Vector3 rangeLeft = rotate * lookVectorNormalized;
-        Handles.DrawLine(transform.position, transform.position + rangeLeft * radius, 3.0f);
-        Handles.DrawWireArc(vecTriggerPos, Vector3.up, rangeLeft, threshold*2, radius, 3.0f);
+        Vector3 offsetTriggerPosDown = new(vecTriggerPos.x, vecTriggerPos.y - wedgeHeight * 0.5f, vecTriggerPos.z);
+        Vector3 offsetTriggerPosUp = new(vecTriggerPos.x, vecTriggerPos.y + wedgeHeight * 0.5f, vecTriggerPos.z);
+
+        //actually drawing the wedge: first three lines draw one slice, second three draw second slice higher up
+        Handles.DrawLine(offsetTriggerPosDown, offsetTriggerPosDown + rangeRight * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosDown, offsetTriggerPosDown + rangeLeft * radius, 3.0f);
+        Handles.DrawWireArc(offsetTriggerPosDown, Vector3.up, rangeLeft, threshold * 2, radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosUp, offsetTriggerPosUp + rangeRight * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosUp, offsetTriggerPosUp + rangeLeft * radius, 3.0f);
+        Handles.DrawWireArc(offsetTriggerPosUp, Vector3.up, rangeLeft, threshold * 2, radius, 3.0f);
+
+        //connect corners
+        Handles.DrawLine(offsetTriggerPosDown + rangeRight * radius, offsetTriggerPosUp + rangeRight * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosDown + rangeLeft * radius, offsetTriggerPosUp + rangeLeft * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosDown, offsetTriggerPosUp, 3.0f);
     }
 }
